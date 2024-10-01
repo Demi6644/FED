1a802695183674c36d2c6d87db8df9950a422cd8
Demi <145349441+Demi6644@users.noreply.github.com>
2024-10-01 12:12:07 +0200

    versie5

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
 console.log("hi");
+
+const items = document.querySelectorAll('.carousel-item');
+let currentItem = 0;
+
+document.getElementById('next').addEventListener('click', () => {
+    items[currentItem].classList.remove('active');
+    currentItem = (currentItem + 1) % items.length;
+    items[currentItem].classList.add('active');
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    items[currentItem].classList.remove('active');
+    currentItem = (currentItem - 1 + items.length) % items.length;
+    items[currentItem].classList.add('active');
+});
