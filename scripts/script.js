4702b3bf2dbae34080f49d461b99da20e5db1632
Demi <145349441+Demi6644@users.noreply.github.com>
2024-10-01 20:46:00 +0200

    versie7

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 console.log("hi");
 
+
+const hamburgerButton = document.querySelector('header button img');
+const menuBody = document.querySelector('nav');
 const items = document.querySelectorAll('.carousel-item');
 let currentItem = 0;
 
@@ -15,3 +18,13 @@ document.getElementById('prev').addEventListener('click', () => {
     currentItem = (currentItem - 1 + items.length) % items.length;
     items[currentItem].classList.add('active');
 });
+
+function closeMenu(){
+    if(menuBody.style.display == "block") {
+        menuBody.style.display = "none";
+   }  else {
+        menuBody.style.display = "block";
+   }
+}
+
+hamburgerButton.addEventListener('click', closeMenu);
